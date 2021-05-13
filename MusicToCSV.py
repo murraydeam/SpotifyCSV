0a0ab353d99facc4817bf6bacb1187076c244ed2
dmurray <deandremurray22@gmail.com>
2021-05-13 15:43:14 -0400

    Json data has been concatenated to 'allData', there is an issue with the format. Var 'allData' will need to be formatted as a string to remove quotes within the var, or I may need to use the quotes as a delimiter to signal new line in CSV reader.

--- MusicToCSV.py
+++ MusicToCSV.py
@@ -21,7 +21,6 @@ messageBytes = message.encode('ascii')
 base64Bytes = base64.b64encode(messageBytes)
 base64Message = base64Bytes.decode('ascii')
 
-
 headers['Authorization'] = f"Basic {base64Message}"
 
 data['grant_type'] = "client_credentials"
@@ -45,23 +44,28 @@ playlist_data = json.loads(playlist_data)
 for key in playlist_data['tracks'].keys():
     print(key)
 """
-fields = ['Title', 'Artist', 'Album']
 
+# Fields for the CSV file "Headers"
+fields = ['Title', 'Artist', 'Album']
+# Define Var as a list
+allData = []
 for album in playlist_data['tracks']['items']:
     for artist in album['track']['album']['artists']:
         #  Get data from the spotify Json (Song name, Artist, Album Name)
-        albumName = album['track']['album']['name']
-        songName = album['track']['name']
-        artistName = artist['name']
+        albumName = album['track']['name'].replace(',', '')
+        trackName = album['track']['album']['name'].replace(',', '')
+        artistName = artist['name'].replace(',', '')
+        trackData = f'{albumName}, {trackName}, {artistName}'
+        # The above will also remove any Comma's from the data for simple csv parsing.
+        # Append 'trackData' to list 'allData'
+        allData.append(trackData)
 
-        #  print(f'\n{songName} by {artistName}\nAlbum : {albumName}')
-
-        datas = 'Song Name: ' + songName + '\nArtist Name: ' + artistName + '\nAlbum Name: ' + albumName
-        print('\n' + datas)
+print(allData)
 with open(playlistID + '.csv', 'w', newline='') as csvfile:
     songwriter = csv.writer(csvfile, delimiter=' ')
+    songwriter.writerow(allData)
+
 """
 for artist in playlist_data['tracks']['items']:
     print(artist['track']['name'])
 """
-
