7b5676cde2d8814b12f9bf8b29eb3f81d0a29be5
deand <Deandremurray22@gmail.com>
2021-12-05 19:30:16 -0500

    Removed UI place holder file, created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SpotifyCSV',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/murraydeam/SpotifyCSV',
+    license='',
+    author='Deandre Murray',
+    author_email='Deandremurray22@gmail.com',
+    description=''
+)
